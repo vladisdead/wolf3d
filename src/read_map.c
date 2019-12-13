6d1780b8f71cac41706d24873db21b86a03d5d60
Carly Yuriko <cyuriko@oa-j1.21-school.ru>
2019-12-13 15:43:21 +0300

    del garb

--- src/read_map.c
+++ src/read_map.c
@@ -6,7 +6,7 @@
 /*   By: cyuriko <cyuriko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/24 15:18:39 by cyuriko           #+#    #+#             */
-/*   Updated: 2019/12/13 14:28:31 by cyuriko          ###   ########.fr       */
+/*   Updated: 2019/12/13 14:33:39 by cyuriko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ t_wmap		*init_map(char *arg)
 	if (!count_data(arg, wolf_map))
 	{
 		wolf_map->map = interpret_buff(arg, wolf_map);
-		ft_strcpy(wolf_map->name, arg);
 		return (wolf_map);
 	}
 	free(wolf_map);
